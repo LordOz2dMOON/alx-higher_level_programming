78cd2533b7714d15cea6a1ed6263ca3b73b28f7b
Oswald Ojo <oswaldojo@ymail.com>
2022-09-21 00:31:08 +0000

    python scripts

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School" * 3
+nine = str[0:10]
+print(str)
+print(nine)
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_three = word[0:3]
+last_two = word[-2:]
+middle = word[1:-1]
+print(f"First 3 letters: {first_three}")
+print(f"Last 2 letters: {last_two}")
+print(f"Middle word: {middle}")
