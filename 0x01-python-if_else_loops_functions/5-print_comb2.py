474cbafc1084e7f84f526bcf12bcc43cce63bc81
Oswald Ojo <oswaldojo@ymail.com>
2022-09-22 04:50:43 +0000

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,100):
     if i == 99:
-        print('{}'.format(i:02),end=" ")
+        print('{:02d}'.format(i),end=" ")
         break
-    print('{}'.format(i:02), sep= ',', end=", ")
+    print('{:02d}'.format(i), sep= ',', end=", ")
