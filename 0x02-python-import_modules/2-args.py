ce5e956a5e53b077f43e995f848e7d8fcf31b9ab
Oswald Ojo <oswaldojo@ymail.com>
2022-09-22 23:59:49 +0000

    dynamaic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     argv_count = len(argv)
     number = 1
     if argv_count == 0:
-        print('{} arguments'.format(argv_count))
+        print('{} arguments.'.format(argv_count))
     elif argv_count == 1:
-        print('{} argument'.format(argv_count))
+        print('{} argument:'.format(argv_count))
         print('{}: {}'.format(number, sys.argv[1]))
     else:
         print('{} arguments'.format(argv_count))
