02f6e7eb1be2eac095683ef769d1b40fcdbd9803
Oswald Ojo <106757524+LordOz2dMOON@users.noreply.github.com>
2022-09-23 00:51:23 +0100

    Add files via upload

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+import sys
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    number = 1
+    if argv_count == 0:
+        print('{} arguments'.format(argv_count))
+    elif argv_count == 1:
+        print('{} argument'.format(argv_count))
+        print('{}: {}'.format(index, sys.argv[1]))
+    else:
+        print('{} arguments'.format(argv_count))
+        while number <= argv_count:
+            print('{}: {}'.format(number, sys.argv[number]))
+            number += 1
