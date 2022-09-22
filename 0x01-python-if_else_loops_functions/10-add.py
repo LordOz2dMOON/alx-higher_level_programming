aefd9a7e27db9fc2cc362121591528893aebf329
Oswald Ojo <oswaldojo@ymail.com>
2022-09-22 05:04:10 +0000

    corrections

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-#create function with Prototype: def add(a, b):
 def add(a, b):
-#return value of a + b
     return a + b
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-#create function with Prototype: def pow(a, b):
 def pow(a, b):
-#return value of a ^ b
     return a ** b
