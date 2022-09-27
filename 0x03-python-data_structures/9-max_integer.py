1b9147f4fbc277dc2af951a1dd2e0b6acaa6837c
Oswald Ojo <106757524+LordOz2dMOON@users.noreply.github.com>
2022-09-27 22:27:00 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+ """
+ find the maximum value of a list
+ Args:
+    my_list - list to search
+ Return:
+    None - if list is empty
+    maximum of list
+ """
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
